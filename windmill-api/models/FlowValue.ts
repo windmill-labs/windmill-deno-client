84e0c4db76e4ca25ca0bbb68c092e1e7fdba760b
windmill-labs <ruben@windmill.dev>
2023-08-28 18:21:45 +0000

    See https://github.com/windmill-labs/windmill/commit/d469802186d28ec6d1d7bad5af614175185bbabe from refs/tags/v1.161.0

--- windmill-api/models/FlowValue.ts
+++ windmill-api/models/FlowValue.ts
@@ -8,5 +8,8 @@ export type FlowValue = {
     modules: Array<FlowModule>;
     failure_module?: FlowModule;
     same_worker?: boolean;
+    concurrent_limit?: number;
+    concurrency_time_window_s?: number;
+    skip_expr?: string;
 };
 
