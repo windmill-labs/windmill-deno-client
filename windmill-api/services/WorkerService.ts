1286941fbbff14acc60a3fdcb31f0427ea4cba8c
windmill-labs <ruben@windmill.dev>
2024-02-06 15:38:42 +0000

    See https://github.com/windmill-labs/windmill/commit/d1f3adade82acea52195a8f4c9442d1a23fb576b from refs/tags/v1.263.0

--- windmill-api/services/WorkerService.ts
+++ windmill-api/services/WorkerService.ts
@@ -29,6 +29,7 @@ export class WorkerService {
     public static listWorkers({
         page,
         perPage,
+        pingSince,
     }: {
         /**
          * which page to return (start at 1, default 1)
@@ -38,6 +39,10 @@ export class WorkerService {
          * number of items to return for a given page (default 30, max 100)
          */
         perPage?: number,
+        /**
+         * number of seconds the worker must have had a last ping more recent of (default to 300)
+         */
+        pingSince?: number,
     }): CancelablePromise<Array<WorkerPing>> {
         return __request(OpenAPI, {
             method: 'GET',
@@ -45,6 +50,7 @@ export class WorkerService {
             query: {
                 'page': page,
                 'per_page': perPage,
+                'ping_since': pingSince,
             },
         });
     }
