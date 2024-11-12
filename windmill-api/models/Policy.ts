b615de790296aa5b4f82e6cfe7300041bc29664f
windmill-labs <ruben@windmill.dev>
2024-11-12 22:42:53 +0000

    See https://github.com/windmill-labs/windmill/commit/c1b220be26145c89ba943da5a48131b2cbd1fdbd from refs/tags/v1.423.0

--- windmill-api/models/Policy.ts
+++ windmill-api/models/Policy.ts
@@ -5,6 +5,7 @@
 export type Policy = {
     triggerables?: Record<string, any>;
     triggerables_v2?: Record<string, any>;
+    s3_inputs?: Array<any>;
     execution_mode?: Policy.execution_mode;
     on_behalf_of?: string;
     on_behalf_of_email?: string;
