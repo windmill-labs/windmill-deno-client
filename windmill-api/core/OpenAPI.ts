f2e19c2c9df3ad461065c0e0dc130c3e0b5ff8bf
windmill-labs <ruben@windmill.dev>
2024-11-13 10:25:21 +0000

    See https://github.com/windmill-labs/windmill/commit/9f7aa01cac09d0c1c57335ddbc80926be85810bf from refs/tags/v1.423.1

--- windmill-api/core/OpenAPI.ts
+++ windmill-api/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 
 export const OpenAPI: OpenAPIConfig = {
     BASE: '/api',
-    VERSION: '1.423.0',
+    VERSION: '1.423.1',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
     TOKEN: undefined,
